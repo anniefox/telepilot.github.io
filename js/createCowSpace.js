23bfbed092bdbddf5efa7a3ce7f5bccf1c137ff6
Teodor Bjerre <teodor.bjerre@gmail.com>
2018-08-27 19:27:51 +0100

    let's see

--- js/createCowSpace.js
+++ js/createCowSpace.js
@@ -148,7 +148,7 @@ if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 
 				var loader2 = new THREE.JSONLoader(loadingManager);
 				loader2.load('models/newcowmilk.json', handle_load2,	function ( xhr ) {
-					if(xhr.total > 0) {
+					if(xhr.loaded / xhr.total >= 0.01) {
 						loadingBar.style.width = Math.floor((xhr.loaded / xhr.total * 100)) + '%';
 						loadingPercentage.innerHTML = Math.floor((xhr.loaded / xhr.total * 100)) + '%'
 						console.log( Math.floor((xhr.loaded / xhr.total * 100)) + '%' );
